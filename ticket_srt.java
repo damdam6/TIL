240a7f706890c19058213feacd5df72f798fe5cf
damdam6 <47710007+damdam6@users.noreply.github.com>
2023-07-14 13:36:52 +0900

    Add files via upload

---
+++ ticket_srt.java
@@ -0,0 +1,39 @@
+package selenium_practice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+//selenium 추가
+//구글드라이버 추가 필요
+
+
+public class ticket_srt {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://etk.srail.kr/main.do");
+		
+		
+		WebElement departureInput = driver.findElement(By.cssSelector("#dptRsStnCd"));
+		Select departure = new Select(departureInput);
+		departure.selectByVisibleText("수서");
+		
+		WebElement arrivalInput = driver.findElement(By.cssSelector("#arvRsStnCd"));
+		Select arrival = new Select(arrivalInput);
+//		arrival.selectByVisibleText("부산");
+		
+		//value 값을 사용하여 지정
+		arrival.selectByValue("0020");
+		
+		WebElement searchBtn = driver.findElement(By.cssSelector("#search-form > fieldset > a"));
+		searchBtn.click();
+		
+
+	}
+
+}
